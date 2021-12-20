0bfcec6c7b5cb46e40f05d650df4ba8b353fb209
marcodeiulio <mdeiulio@icloud.com>
2021-12-20 16:17:38 +0100

    - Added password generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log('JS OK');
+
+/*
+Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+/*
+Scaletta:
+- Chiedi nome utente
+- Conserva nome utente
+- Chiedi cognome utente
+- Conserva cognome utente
+- Chiedi colore preferito
+- Conserva cognome preferito
+- Consegna password
+*/
+
+const userName = prompt("Come ti chiami?");
+console.log(userName);
+
+const userSurname = prompt(userName + " come?");
+console.log(userSurname);
+
+const userColor = prompt("Qual è il tuo colore preferito?")
+console.log(userColor);
+
+const psw = `${userName}${userSurname}21`;
+console.log(psw);
